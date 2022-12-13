39215a3ce0804cfbd714d07ba663236a1af83d63
Colin Fortuner <cfortuner@gmail.com>
2022-12-13 12:31:43 -0500

    add tooltip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,11 +43,29 @@ const Home: NextPage = () => {
                 <div>USER ID</div>
                 {userId}
               </div>
-              <AutoSaveInput
-                onSubmit={handleSubmit}
-                onCancel={() => null}
-                placeHolder="Prompt Id"
-              />
+              <div className="flex items-center space-x-1">
+                <AutoSaveInput
+                  onSubmit={handleSubmit}
+                  onCancel={() => null}
+                  placeHolder="Prompt Id"
+                />
+                <div className="">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+                    />
+                  </svg>
+                </div>
+              </div>
             </div>
             <div className="py-4">
               <ThemeSelect />
