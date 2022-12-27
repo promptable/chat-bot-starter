cdf6012cc0fd4be9e652a9bdea855aa9ffd2ab69
Colin Fortuner <cfortuner@gmail.com>
2022-12-26 21:49:15 -0500

    default theme to fix the render bug

--- src/components/ui/Theme/ThemeProvider.tsx
+++ src/components/ui/Theme/ThemeProvider.tsx
@@ -1,8 +1,19 @@
 import { themeAtom } from "@components/ui/Theme/ThemeSelect";
 import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
 
 export default function ThemeProvider(props: { children: React.ReactNode }) {
   const [theme, setTheme] = useAtom(themeAtom);
+
+  // https://jotai.org/docs/utils/atom-with-storage#server-side-rendering
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  if (!hasMounted) {
+    return null;
+  }
 
   return (
     <div data-theme={theme} className="h-full">
